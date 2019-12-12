xingxingzhayan/android-1

test CreateFolderOperation without server
Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

src/main/java/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
@@ -399,7 +399,7 @@ public final class ThumbnailsCacheManager {
         private List<ThumbnailGenerationTask> mAsyncTasks;
         private Object mFile;
         private String mImageKey;
-        private FileDataStorageManager mStorageManager;
+        private FileDataStorageManagerInterface mStorageManager;
         private GetMethod getMethod;
 
         public ThumbnailGenerationTask(ImageView imageView, FileDataStorageManager storageManager, Account account)
@@ -424,7 +424,7 @@ public final class ThumbnailsCacheManager {
             return getMethod;
         }
 
-        public ThumbnailGenerationTask(FileDataStorageManager storageManager, Account account){
+        public ThumbnailGenerationTask(FileDataStorageManagerInterface storageManager, Account account) {
             if (storageManager == null) {
                 throw new IllegalArgumentException("storageManager must not be NULL");
             }

src/test/java/com/owncloud/android/FileDataStorageManagerLocal.java
@@ -1,0 +1,149 @@
+package com.owncloud.android;
+
+import android.accounts.Account;
+
+import com.owncloud.android.datamodel.FileDataStorageManagerInterface;
+import com.owncloud.android.datamodel.OCFile;
+import com.owncloud.android.lib.resources.shares.OCShare;
+import com.owncloud.android.lib.resources.shares.ShareType;
+import com.owncloud.android.lib.resources.status.OCCapability;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import androidx.annotation.Nullable;
+
+public class FileDataStorageManagerLocal implements FileDataStorageManagerInterface {
+    private Map<String, OCFile> pathToFile = new HashMap<>();
+    private long currentMaxId = 0;
+
+    public FileDataStorageManagerLocal() {
+        // always have root file
+        pathToFile.put("/", createOCFile("/"));
+    }
+
+    @Override
+    public OCFile getFileByPath(String path) {
+        return pathToFile.get(path);
+    }
+
+    @Override
+    public Account getAccount() {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Nullable
+    @Override
+    public OCFile getFileById(long id) {
+        for (Map.Entry<String, OCFile> entry : pathToFile.entrySet()) {
+            if (entry.getValue().getFileId() == id) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void saveConflict(OCFile file, String etagInConflict) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void deleteFileInMediaScan(String path) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean saveFile(OCFile file) {
+        pathToFile.put(file.getRemotePath(), file);
+
+        return true;
+    }
+
+    @Override
+    public boolean fileExists(long id) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public List<OCFile> getFolderContent(OCFile f, boolean onlyOnDevice) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void saveFolder(OCFile folder, Collection<OCFile> updatedFiles, Collection<OCFile> filesToRemove) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean removeFile(OCFile file, boolean removeDBData, boolean removeLocalCopy) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean removeFolder(OCFile folder, boolean removeDBData, boolean removeLocalContent) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void moveLocalFile(OCFile file, String targetPath, String targetParentPath) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean saveShare(OCShare share) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public List<OCShare> getSharesWithForAFile(String filePath, String accountName) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void removeShare(OCShare share) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void copyLocalFile(OCFile file, String targetPath) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public OCShare getFirstShareByPathAndType(String path, ShareType type, String shareWith) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public OCCapability saveCapabilities(OCCapability capability) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void saveSharesDB(List<OCShare> shares) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void removeSharesForFile(String remotePath) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public OCShare getShareById(long id) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    public OCFile createOCFile(String path) {
+        OCFile file = new OCFile(path);
+
+        file.setFileId(currentMaxId);
+        currentMaxId++;
+
+        return file;
+    }
+}

src/test/java/com/owncloud/android/operations/CreateFolderOperationTest.java
@@ -1,0 +1,73 @@
+package com.owncloud.android.operations;
+
+import com.owncloud.android.FileDataStorageManagerLocal;
+import com.owncloud.android.datamodel.FileDataStorageManagerInterface;
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+import com.owncloud.android.lib.resources.files.CreateFolderRemoteOperation;
+import com.owncloud.android.lib.resources.files.ReadFolderRemoteOperation;
+import com.owncloud.android.lib.resources.files.model.RemoteFile;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class CreateFolderOperationTest {
+    private FileDataStorageManagerInterface fileDataStorageManager = new FileDataStorageManagerLocal();
+
+    @Mock OwnCloudClient client;
+
+    @Mock CreateFolderRemoteOperation createFolderRemoteOperation;
+    @Mock ReadFolderRemoteOperation readFolderRemoteOperation;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testCreateFolder() {
+        // setup
+        String path = "/testFolder/";
+        CreateFolderOperation sut = spy(new CreateFolderOperation(path, true));
+
+        when(createFolderRemoteOperation.execute(any()))
+            .thenReturn(new RemoteOperationResult(RemoteOperationResult.ResultCode.OK));
+
+        RemoteOperationResult result = new RemoteOperationResult(RemoteOperationResult.ResultCode.OK);
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(new RemoteFile("/testFolder"));
+
+        result.setData(list);
+
+        when(readFolderRemoteOperation.execute(any(OwnCloudClient.class), any(Boolean.class)))
+            .thenReturn(result);
+        doReturn(createFolderRemoteOperation).
+            when(sut).createCreateFolderRemoteOperation(any(String.class), any(Boolean.class));
+        doReturn(readFolderRemoteOperation)
+            .when(sut).createReadFolderRemoteOperation(any(String.class));
+
+        // tests
+        // folder does not exist yet
+        assertNull(fileDataStorageManager.getFileByPath(path));
+
+        // run
+        RemoteOperationResult syncResult = sut.execute(client, fileDataStorageManager);
+
+        // verify
+        assertTrue(syncResult.isSuccess());
+
+        // folder exists
+        assertTrue(fileDataStorageManager.getFileByPath(path).isFolder());
+    }
+}

grote/nextcloud-android

src/main/java/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
@@ -433,7 +433,7 @@ public final class ThumbnailsCacheManager {
             return getMethod;
         }
 
-        public ThumbnailGenerationTask(FileDataStorageManager storageManager, Account account){
+        public ThumbnailGenerationTask(FileDataStorageManager storageManager, Account account) {
             if (storageManager == null) {
                 throw new IllegalArgumentException("storageManager must not be NULL");
             }

src/test/java/com/owncloud/android/FileDataStorageManagerLocal.java
@@ -1,0 +1,149 @@
+package com.owncloud.android;
+
+import android.accounts.Account;
+
+import com.owncloud.android.datamodel.FileDataStorageManager;
+import com.owncloud.android.datamodel.OCFile;
+import com.owncloud.android.lib.resources.shares.OCShare;
+import com.owncloud.android.lib.resources.shares.ShareType;
+import com.owncloud.android.lib.resources.status.OCCapability;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import androidx.annotation.Nullable;
+
+public class FileDataStorageManagerLocal implements FileDataStorageManager {
+    private Map<String, OCFile> pathToFile = new HashMap<>();
+    private long currentMaxId = 0;
+
+    public FileDataStorageManagerLocal() {
+        // always have root file
+        pathToFile.put("/", createOCFile("/"));
+    }
+
+    @Override
+    public OCFile getFileByPath(String path) {
+        return pathToFile.get(path);
+    }
+
+    @Override
+    public Account getAccount() {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Nullable
+    @Override
+    public OCFile getFileById(long id) {
+        for (Map.Entry<String, OCFile> entry : pathToFile.entrySet()) {
+            if (entry.getValue().getFileId() == id) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void saveConflict(OCFile file, String etagInConflict) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void deleteFileInMediaScan(String path) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean saveFile(OCFile file) {
+        pathToFile.put(file.getRemotePath(), file);
+
+        return true;
+    }
+
+    @Override
+    public boolean fileExists(long id) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public List<OCFile> getFolderContent(OCFile f, boolean onlyOnDevice) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void saveFolder(OCFile folder, Collection<OCFile> updatedFiles, Collection<OCFile> filesToRemove) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean removeFile(OCFile file, boolean removeDBData, boolean removeLocalCopy) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean removeFolder(OCFile folder, boolean removeDBData, boolean removeLocalContent) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void moveLocalFile(OCFile file, String targetPath, String targetParentPath) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean saveShare(OCShare share) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public List<OCShare> getSharesWithForAFile(String filePath, String accountName) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void removeShare(OCShare share) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void copyLocalFile(OCFile file, String targetPath) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public OCShare getFirstShareByPathAndType(String path, ShareType type, String shareWith) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public OCCapability saveCapabilities(OCCapability capability) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void saveSharesDB(List<OCShare> shares) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void removeSharesForFile(String remotePath) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public OCShare getShareById(long id) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    public OCFile createOCFile(String path) {
+        OCFile file = new OCFile(path);
+
+        file.setFileId(currentMaxId);
+        currentMaxId++;
+
+        return file;
+    }
+}

src/test/java/com/owncloud/android/operations/CreateFolderOperationTest.java
@@ -1,0 +1,72 @@
+package com.owncloud.android.operations;
+
+import com.owncloud.android.FileDataStorageManagerLocal;
+import com.owncloud.android.datamodel.FileDataStorageManager;
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+import com.owncloud.android.lib.resources.files.CreateFolderRemoteOperation;
+import com.owncloud.android.lib.resources.files.ReadFolderRemoteOperation;
+import com.owncloud.android.lib.resources.files.model.RemoteFile;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class CreateFolderOperationTest {
+    private FileDataStorageManager fileDataStorageManager = new FileDataStorageManagerLocal();
+
+    @Mock OwnCloudClient client;
+
+    @Mock CreateFolderRemoteOperation createFolderRemoteOperation;
+    @Mock ReadFolderRemoteOperation readFolderRemoteOperation;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testCreateFolder() {
+        // setup
+        String path = "/testFolder/";
+        CreateFolderOperation sut = spy(new CreateFolderOperation(path, true));
+
+        when(createFolderRemoteOperation.execute(any()))
+            .thenReturn(new RemoteOperationResult(RemoteOperationResult.ResultCode.OK));
+
+        RemoteOperationResult result = new RemoteOperationResult(RemoteOperationResult.ResultCode.OK);
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(new RemoteFile("/testFolder"));
+
+        result.setData(list);
+
+        when(readFolderRemoteOperation.execute(any(OwnCloudClient.class))).thenReturn(result);
+        doReturn(createFolderRemoteOperation).
+            when(sut).createCreateFolderRemoteOperation(any(String.class), any(Boolean.class));
+        doReturn(readFolderRemoteOperation)
+            .when(sut).createReadFolderRemoteOperation(any(String.class));
+
+        // tests
+        // folder does not exist yet
+        assertNull(fileDataStorageManager.getFileByPath(path));
+
+        // run
+        RemoteOperationResult syncResult = sut.execute(client, fileDataStorageManager);
+
+        // verify
+        assertTrue(syncResult.isSuccess());
+
+        // folder exists
+        assertTrue(fileDataStorageManager.getFileByPath(path).isFolder());
+    }
+}